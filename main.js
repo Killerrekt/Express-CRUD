36caad0a21eece233a026f626a46693f71ce048b
Killerrekt <amankhanter14@gmail.com>
2024-03-13 17:53:35 +0530

    feat: db connection establish

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const uri = process.env.URI
+mongoose.connect(uri).then(() => console.log("Successfully Connected to Mongo")).catch((err) => console.log(err))
 
 const app = express()
 const port = 3000
